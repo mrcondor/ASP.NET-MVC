c51d09ef273cc87daace02e463fbb7c0191b0404
Rodolpho Rangel de Barros <mr_condor_@hotmail.com>
2018-06-03 00:20:05 -0300

    Sistema com o básico de Bootstrap e JQuery

---
+++ MyWebApp/MyWebApp/wwwroot/js/Index.js
@@ -0,0 +1,30 @@
+﻿//JQUERY
+
+$(document).ready(function () {
+    var $button = $("#meu-button");
+    $button.on("click", () => alert("Alerta"));
+
+    var $precos = $(".preco-produto li");
+    $precos.on("click", function () {
+        alert($(this).text());
+    });
+
+    var $login = $("#login");
+    var $popupForm = $(".popup-form");
+
+    $login.on('click', function () {
+        $popupForm.toggle(500);
+    });
+});
+
+//JAVA SCRIPT
+
+//var meuForm = document.getElementById("meu-form");
+//meuForm.hidden = true;
+
+//var meuButton = document.getElementById("meu-button");
+//meuButton.addEventListener('click', exibirMesnagem);
+
+//function exibirMesnagem() {
+//    alert('Teste Evento');
+//}

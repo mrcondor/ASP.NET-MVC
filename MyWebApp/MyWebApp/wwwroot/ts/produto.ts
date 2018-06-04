7b85f2a10aa0318e3eb8eb6df5498282f4e7f16c
Rodolpho Rangel de Barros <mr_condor_@hotmail.com>
2018-06-03 23:24:06 -0300

    Adicionado Modelos de TypeScript ao projeto

---
+++ MyWebApp/MyWebApp/wwwroot/ts/produto.ts
@@ -0,0 +1,22 @@
+﻿export class Produto {
+    public id: number;
+    public nome: string;
+
+    constructor(private sigla: string, private preco: number) {
+
+    }
+
+    public exibirSigla() {
+        alert(this.sigla);
+    }
+
+    public obterNome(): string {
+        return this.nome;
+    }
+    public obterId(): number {
+        return this.id;
+    }
+
+    set descricao(val) { }
+    get descricao() { return this.nome; }
+}
